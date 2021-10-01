511b581dc1261f4ac56fb16543c2ca2ab7f56018
Guilherme Anzolin Iaquinto <85501131+guiiaquinto@users.noreply.github.com>
2021-10-01 03:21:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-let produtos = require ("./dados");
+let produtos = require("./dados");
 
-//função para filtrar os produtos e armazenar no carrinho
-const carrinho = produtos.filter(produto => produto.preco >= 482 && produto.preco <= 1600 && produto.qualidade > 6 && produto.status === true)
+let carrinho = produtos.filter(produto => produto.preco >= 482 && produto.preco <= 1600 && produto.qualidade > 6 && produto.status === true)
 
-//função para mostrar os produtos selecinados e seu total
-function printInfoCarrinho(){
-    console.log('Produtos no carrinho:\n');
 
-    carrinho.forEach(produto=>{
+function resumoDoCarrinho() {
+    console.log("Produtos no carrinho:");
+
+    carrinho.forEach(produto => {
         console.log(`Produto: ${produto.nome}`)
-        console.log(`Preço: R$ ${produto.preco.toFixed(2)}\n`)
+        console.log(`Preço: R$ ${produto.preco}`)
     });
-    console.log(`Total: RS ${carrinho.reduce((acum,produto)=>acum+produto.preco,0).toFixed(2)}\n`);
+
+    console.log(`O total é: R$ ${carrinho.reduce((acc, produto) => acc + produto.preco, 0)}`);
 }
 
+
+resumoDoCarrinho();
 console.table(carrinho);
-printInfoCarrinho();
