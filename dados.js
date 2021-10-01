42e2c76b40c7f3e077765f450738eade179055ab
Guilherme Anzolin Iaquinto <85501131+guiiaquinto@users.noreply.github.com>
2021-09-30 23:01:38 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,52 @@
+let produtos = [
+    {
+        "nome": "Nike PG5", 
+        "preco": 600, 
+        "qualidade": 8, 
+        "status": true
+    },
+    {
+        "nome": "Nike LeBron IXX", 
+        "preco": 900,
+        "qualidade": 9, 
+        "status": false
+    },
+    {
+        "nome": "Air Jordan XXXV",
+        "preco": 999, 
+        "qualidade": 10, 
+        "status": true
+    },
+    {
+        "nome": "Nike KD XII", 
+        "preco": 750, 
+        "qualidade": 7, 
+        "status": true
+    },
+    {
+        "nome": "Adidas Harden 5", 
+        "preco": 650, 
+        "qualidade": 6, 
+        "status": true
+    },
+    {
+        "nome": "Nike Precision IV", 
+        "preco": 250, 
+        "qualidade": 4, 
+        "status": false
+    },
+    {
+        "nome": "Nike Lebron Witeness V", 
+        "preco": 499, 
+        "qualidade": 4, 
+        "status": true
+    },
+    {
+        "nome": "Adidas Pro Next 2019", 
+        "preco": 200, 
+        "qualidade": 5, 
+        "status": true
+    }
+];
+
+module.exports = produtos;
